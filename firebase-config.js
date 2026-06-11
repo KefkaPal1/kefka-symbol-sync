e3af0f3a40b5cae32ade7aa9010c889f5e4319a5
KefkaPal1 <meseoul01@gmail.com>
2026-06-11 12:54:49 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC-dvs72ZqJ9joB8eqdG6t2_lCSWCczi3s",
+  authDomain: "kefka-symbol-sync.firebaseapp.com",
+  databaseURL: "https://kefka-symbol-sync-default-rtdb.firebaseio.com",
+  projectId: "kefka-symbol-sync",
+  storageBucket: "kefka-symbol-sync.firebasestorage.app",
+  messagingSenderId: "668813110944",
+  appId: "1:668813110944:web:11c718be23fd49ff7ca3c5"
+};
